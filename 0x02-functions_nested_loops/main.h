26b4d7fb357594be103697c1dd363dccdd96d404
AzubuikeThankGod1 <thankstrinity9@gmail.com>
2022-06-08 06:59:10 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,12 @@
+#ifdef MAIN_H
+#define MAIN_H
+
+/*
+ * _putchar prototype
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
 int _putchar(char c);
+
+#endif
