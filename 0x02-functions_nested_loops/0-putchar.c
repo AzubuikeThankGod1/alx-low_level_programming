6599541e067f62eace62fc6d2ccdfdb290c9f7d6
AzubuikeThankGod1 <thankstrinity9@gmail.com>
2022-06-07 20:14:49 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,6 +3,7 @@
 /** main - prints putchar
  * Return: always 0
  */
+
 int main(void)
 {
 	_putchar("_");
@@ -15,4 +16,4 @@ int main(void)
 	_putchar("r");
 	_putchar("\n");
 
-	return (0)
+	return (0);
