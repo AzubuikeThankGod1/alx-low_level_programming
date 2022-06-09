bc0de7511df06af5df297477a8d4b7a8708a263a
AzubuikeThankGod1 <thankstrinity9@gmail.com>
2022-06-09 04:13:54 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,6 @@
 #ifdef MAIN_H
 #define MAIN_H
 
-/*
- * main - Entry point
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int main(void);
+int _putchar(char c)
 
 #endif
