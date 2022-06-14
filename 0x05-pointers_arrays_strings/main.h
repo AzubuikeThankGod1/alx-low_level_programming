331b560ad8080c487dc885927e6470e5d6d1130e
AzubuikeThankGod1 <thankstrinity9@gmail.com>
2022-06-13 22:05:21 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -17,5 +17,6 @@ char *_strcpy(char *dest, char *src);
 void print_rev(char *s);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 #endif /* MAIN_H */
